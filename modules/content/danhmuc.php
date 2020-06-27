65909a1fe05ed558a500da5afed17477af8dd433
vudinhcao <50055868+vudinhcao@users.noreply.github.com>
2020-06-27 07:51:32 +0700

    Initial commit

---
+++ modules/content/danhmuc.php
@@ -0,0 +1,26 @@
+<div class="">
+<!-- 	<div class="boxleft">
+		<div class="titboxl">
+		</div>
+		<div class="ctboxleft">
+			
+		</div>
+	</div> -->
+</div>
+<div class="">
+	<div class="slider-wrapper theme-default">
+        <div id="slider" class="nivoSlider">
+			<img src="images/slideshow_1.jpg" alt=""/>
+			<img src="images/slideshow_2.jpg" alt=""/>
+			<img src="images/slideshow_3.png" alt=""/>
+		</div>
+    </div>
+</div>
+<div class="banner clearfix">
+	<!-- <div class="col-xs-12 col-sm-6">
+		<img src="images/banner1.png" alt="">
+	</div>
+	<div class="col-xs-12 col-sm-6">
+		<img src="images/banner2.png" alt="">
+	</div> -->
+</div>
